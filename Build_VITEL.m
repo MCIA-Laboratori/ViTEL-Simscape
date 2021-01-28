b4ba69f7ec57388150557ca18973d408139372a8
Carles <carles.colls@upc.edu>
2021-01-28 18:02:49 +0100

    Typos and vocabulary Corrections.
    Basically vocabulary corrections.

--- Build_VITEL.m
+++ Build_VITEL.m
@@ -1,28 +1,22 @@
-% Para que el ssc_build Funcione hace falta colocar la "libreria" en el
-% siguiente directorio, es distinto para cada MATLAB:
+% For old MATLAB Versions:
+% ssc_build only works if the library is in the following folder:
 
-% Matlab 2015a:
+% MATLAB 2015a:
 % C:\Program Files\MATLAB\2015a\toolbox\physmod\simscape\simscapedemos
 
-% Matlab 2015b:
+% MATLAB 2015b:
 % C:\Program Files\MATLAB\R2015b\toolbox\physmod\simscape\simscapedemos
 
-% MATLAB 2017:
-% No hace falta colocar la libreria en ningun sitio.
+% MATLAB 2017 & Later:
+% No need to run the builder at any specific folder.
 
-%% Luego se escribe lo siguiente:
+%% Commands:
 
-%matlabroot
 ssc_build ViTEL
 
-% Ahora ya estan disponibles en simscape.
+% open ViTEL_lib.slx
 
-% 1. Editar las librerias DESDE EL DIRECTORIO DE DEMOS
-% 2. Compilar Librerias
-% 3. Probar cambios en tu modelo SIMSCAPE (no hace falta reemplazar cajas
-% si no cambia el nombre de la caja).
-%open ViTEL_lib.slx
 
-%% Comentarios:
-% Se puede editar el codigo y debugear siempre que la carpeta +Libreria
-% este en el mismo directorio que el fichero Libreria_lib.slx
+%% Notes:
+% You can edit and debug the code if the library (.slx) and the "+Folder"
+% are on the same root folder.
